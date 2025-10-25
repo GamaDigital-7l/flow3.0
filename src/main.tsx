f9c848068da532c31b247e3b630d10dfda073b84
[dyad] <gustavogama099@gmail.com>
2025-10-25 16:25:01 -0300

    [dyad] Wrapping the application with SessionContextProvider to fix the useSession hook error. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import "./globals.css";
 import { pdfjs } from "react-pdf";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "next-themes"; // Importar ThemeProvider do next-themes
+import { SessionContextProvider } from "@/integrations/supabase/auth";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="dark" attribute="class" storageKey="vite-ui-theme">
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <App />
-    </BrowserRouter>
+    <SessionContextProvider>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <App />
+      </BrowserRouter>
+    </SessionContextProvider>
   </ThemeProvider>
 );
