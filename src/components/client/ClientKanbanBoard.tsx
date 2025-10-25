e22a6580d3950c91166d253f7147abcb1085e4f2
[dyad] <gustavogama099@gmail.com>
2025-10-25 11:18:26 -0300

    [dyad] Fixing import path for KanbanColumn by explicitly specifying the file extension. - wrote 1 file(s)

--- src/components/client/ClientKanbanBoard.tsx
+++ src/components/client/ClientKanbanBoard.tsx
@@ -10,7 +10,7 @@ import { PlusCircle, Loader2, Send, Copy, MessageSquare, X, ChevronLeft, Chevron
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
 import { DIALOG_CONTENT_CLASSNAMES } from '@/lib/constants';
 import ClientTaskCard from './ClientTaskCard';
-import KanbanColumn from './KanbanColumn';
+import KanbanColumn from './KanbanColumn.tsx';
 import ClientMonthSelector from './ClientMonthSelector';
 import { useClientKanban, ClientKanbanHook } from '@/hooks/useClientKanban';
 import { ClientTaskStatus, ClientTask } from '@/types/client';
