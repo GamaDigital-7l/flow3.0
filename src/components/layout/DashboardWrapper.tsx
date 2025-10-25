28b1bd9c31c30c32f75cc8728357db0205789727
[dyad] <gustavogama099@gmail.com>
2025-10-24 22:23:25 -0300

    Reverted all changes back to version c7793a4b2e7a4a0c7094103430d635895e5a86a3

--- src/components/layout/DashboardWrapper.tsx
+++ src/components/layout/DashboardWrapper.tsx
@@ -11,7 +11,7 @@ const DashboardWrapper: React.FC<DashboardWrapperProps> = ({ children }) => {
     <div className={cn(
       "w-full mx-auto",
       "max-w-screen-xl", // Exemplo de max-width (1280px)
-      "px-2 sm:px-3 lg:px-4" // Padding responsivo reduzido
+      "px-3 sm:px-4 lg:px-6" // Padding responsivo: 12px (mobile) a 24px (desktop)
     )}>
       {children}
     </div>
